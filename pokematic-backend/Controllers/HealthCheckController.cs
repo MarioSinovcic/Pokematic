2252ead73027e1010dc5760f8a6f77de3b5bc099
Sean Spires <seanspires123@gmail.com>
2020-03-24 11:52:49 +1300

    Initalised ASP .Net Core Web API

---
+++ pokematic-backend/Controllers/HealthCheckController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pokematic_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        public HealthCheckController()
+        {
+            // TO DO
+        }
+
+        [HttpGet]
+        public ActionResult GetHealthStatus()
+        {
+            return Ok("API online");
+        }
+    }
+}
