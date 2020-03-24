47b5266d72dad391f1a709b39cbbe4ab61e6c410
Sean Spires <seanspires123@gmail.com>
2020-03-24 17:04:44 +1300

    set up inital web API with gitignore

---
+++ pokematic-backend/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pokematic_backend.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetSystemStatus()
+        {
+            return Ok("API online");
+        }
+    }
+}
