2c1f8a71d0d02e296b2d5c9c7dcbd40540bbce17
Mario Sinovcic <38930019+MarioSinovcic@users.noreply.github.com>
2020-03-26 23:04:46 +1300

    Major Frontend Work Finished

--- pokematic-frontend/src/pages/components/PokedexItem.js
+++ pokematic-frontend/src/pages/components/PokedexItem.js
@@ -4,8 +4,21 @@ import './PokedexItem.css'
 function PokedexItem() {
   return (
     <div className="PokedexItem">
-        <div className="pokemon-name">
-          Pokemon Name
+      <div className="pokeball-background"></div>
+      {/* <img src={require('../../images/background_pokeball.png')} alt="pokeball" /> */}
+      <div className="background-enabler">
+        <div className="pokemon-number">
+            #007
+          </div>
+          <div className="pokemon-name">
+            Wartortle
+          </div>
+          <div className="pokemon-image-wrapper">
+            <img className="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png" alt="Wartortle"></img>
+          </div>
+          <div className="pokemon-type">
+            WATER
+          </div>
         </div>
     </div>
   );
--- pokematic-frontend/src/pages/components/PokedexList.js
+++ pokematic-frontend/src/pages/components/PokedexList.js
@@ -3,21 +3,22 @@ import PokedexItem from './PokedexItem'
 import './PokedexList.css'
 
 function PokedexList() {
+
+  // This a temporary solution for rendering all of the pokemon cards
+  var fakePokemon = [];
+  for (var i = 0; i < 150; i++) {
+    fakePokemon.push(i)
+  }
+
+  var pokemonToRender = fakePokemon.map(item => <div class="grid-item"><PokedexItem/></div>)
+
   return (
     <div className="PokedexList">
-      <div class="grid-container">
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
-        <div class="grid-item"><PokedexItem/></div>
+      <div className="pokeball-background-black"></div>
+      <div className="background-enabler">
+        <div class="grid-container">
+          {pokemonToRender}
+        </div>
       </div>
     </div>
   );
