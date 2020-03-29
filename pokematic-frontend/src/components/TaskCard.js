efd3a7ab1d3a20185fc30f02dfcc4da5ce3a3c7b
Vanessa Ciputra <vee.christella@gmail.com>
2020-03-29 20:10:48 +1300

    Add story points and assignees to tasks

--- pokematic-frontend/src/components/TaskCard.js
+++ pokematic-frontend/src/components/TaskCard.js
@@ -5,22 +5,27 @@ import styles from './styledComponents.css';
 import Label from './Label';
 import StatusDropdown from './StatusDropdown';
 import { sizing } from '@material-ui/system';
+import Assignees from './Assignees';
+
+
 
 function TaskCard() {
     // These should be passed in as props
     const taskID = "34"
     const taskTitle = "Implement collaboration feature";
+    const storyPoint = "3";
 
 
     return (
         <div class="TaskCard">
             <div className="TaskHeaders">
             <Typography className="TaskTitleText">{taskTitle}</Typography>
-                <Typography className="TaskIDText">#{taskID}</Typography>
+                <Typography className="TaskIDText StoryPoint">{storyPoint}</Typography>
             </div>
+            <Assignees />
             <div className="TaskLabels">
                 <Label />
-            <StatusDropdown />
+                <StatusDropdown />
 
             </div>
             
