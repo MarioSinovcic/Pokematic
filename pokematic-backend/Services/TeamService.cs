9b1ec7ad9f86f693244b50cd775134b9a0b4810b
Mario Sinovcic <38930019+MarioSinovcic@users.noreply.github.com>
2020-04-01 11:37:34 +1300

    Create and update operations added for teams

---
+++ pokematic-backend/Services/TeamService.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using pokematic_backend.Contexts;
+using pokematic_backend.Interfaces;
+using pokematic_backend.Models;
+
+namespace pokematic_backend.Services
+{
+    public class TeamService : ITeamService
+    {
+        private readonly IMongoCollection<Team> _teams;
+
+        public TeamService(IConfiguration configuration)
+        {
+            var databaseContext = new DatabaseContext(configuration);
+            _teams = databaseContext.Database.GetCollection<Team>("Teams");
+        }
+
+        public string GetAllTeams()
+        {
+            return _teams.ToJson();
+        }
+
+        public Task<Team> Get(string name)
+        {
+            var teamsAsQueryable = _teams.AsQueryable();
+            var teamToGet = teamsAsQueryable.FirstAsync(team => team.Name == name);
+            return teamToGet;
+        }
+
+        public void Create(Team team)
+        {
+            _teams.InsertOneAsync(team);
+        }
+
+        public void Update(ObjectId id, Team teamToUpdate)
+        {
+            _teams.ReplaceOneAsync(team => team.Id == id, teamToUpdate);
+        }
+
+        public void Remove(string name)
+        {
+            _teams.DeleteOneAsync(team => team.Name== name);
+        }
+    }
+}
