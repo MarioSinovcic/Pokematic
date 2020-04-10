8358bd559b2836a824294a565d2ea6e480cf8ec8
Mario Sinovcic <38930019+MarioSinovcic@users.noreply.github.com>
2020-04-10 15:55:23 +1200

    Revert "Revert "Feature/issue 37 implement profile page (#41)" (#44)" (#45)
    This reverts commit 9f552beda0ee499d9036ef355ce6e99170516f7b.

--- pokematic-frontend/src/pages/Board.js
+++ pokematic-frontend/src/pages/Board.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Sidebar from './board-components/Sidebar';
+import Sidebar from '../shared-components/Sidebar';
 import TeamCard from '../shared-components/TeamCard';
 import StatusCard from './board-components/StatusCard';
 import './Board.css'
-import NewTaskButton from './board-components/NewTaskButton';
+import ModalButton from '../shared-components/ModalButton';
 import Header from '../shared-components/Header';
+import AddIcon from '@material-ui/icons/Add';
+
 
 
 function Board() {
+  // Temporary goals list - API call should go here
+  const goals = ['Planning', 'Frontend Team', 'Testers', 'Design Squad'];
+  const sidebarTitle = 'ALL TASKS';
+  const sidebarSubTitle = 'MY TASKS';
+
+  const newTaskIcon = <AddIcon style={{fontSize: "35px"}}/>;
+
   return (
     <div>
       <div className="board-page">
@@ -17,7 +26,7 @@ function Board() {
             <TeamCard />
       </div>
         <div className="menu">
-          <Sidebar />
+          <Sidebar items={goals} title={sidebarTitle} subTitle={sidebarSubTitle} itemType="GOAL"/>
         </div>
         <div className="tasks-content">
           <div className="todo-status">
@@ -28,7 +37,9 @@ function Board() {
           <StatusCard id="Done"/>
           </div>
         </div>
-        <NewTaskButton />
+        <div className="new-task-button">
+          <ModalButton icon={newTaskIcon} theme="dark" type="new-task"/>
+        </div>
       </div>
         {/* Example: This is how we should use Navlinks to swap between routes in nested components */}
         <NavLink to="/login" >Log Out</NavLink>
--- pokematic-frontend/src/pages/Profile.js
+++ pokematic-frontend/src/pages/Profile.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import Header from '../shared-components/Header';
+import Sidebar from '../shared-components/Sidebar';
+import ProfileCard from './profile-components/ProfileCard';
+import './Profile.css';
+import ModalButton from '../shared-components/ModalButton';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Profile() {
+
+// Temporary teams list - API call should go here
+const teams = ['yes', 'hello'];
+const sidebarTitle = 'TEAMS';
+
+const searchTeamIcon = <SearchIcon style={{fontSize: "35px", color: "#3D3D3D"}}/>;
+const newTeamIcon = <AddIcon style={{fontSize: "35px", color: "#3D3D3D"}}/>;
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <p>
-          Profile
-        </p>
+      <Header />
+      
+      <div className="profile-background"></div>
+        <div className="menu">
+          <Sidebar items={teams} title={sidebarTitle} itemType="TEAM"/>
+          <div className="team-buttons">
+            <ModalButton icon={searchTeamIcon} theme="light" type="search-team"/>
+            <ModalButton icon={newTeamIcon} theme="light" type="new-team"/>
+          </div>
+        </div>
+        <div className="profile-content">
+          <ProfileCard className="profile-card"/>
+        </div>
       </header>
     </div>
   );
