314e4c9ad6971d51e3e7ad7529d96da766a52292
Vanessa Ciputra <vee.christella@gmail.com>
2020-04-16 16:29:42 +1200

    add type enums and start color

---
+++ pokematic-frontend/src/colors.js
@@ -0,0 +1,1 @@
+export const fireRed = "#F64747";
--- pokematic-frontend/src/constants.js
+++ pokematic-frontend/src/constants.js
@@ -3,3 +3,23 @@ export const STATUSLIST = ["TODO", "In Progress", "In Review", "Done"];
 
 export const GETGOALS = "";
 export const GETTASKS = "";
+
+export const POKEMONTYPES = {
+    FIRE: "fire",
+    WATER: "water",
+    GRASS: "grass",
+    POISON: "poison",
+    FLYING: "flying",
+    BUG: "bug",
+    NORMAL: "normal",
+    ELECTRIC: "electric",
+    GROUND: "ground",
+    FAIRY: "fairy",
+    FIGHTING: "fighting",
+    PSYCHIC: "psychic",
+    ROCK: "rock",
+    STEEL: "steel",
+    ICE: "ice",
+    GHOST: "ghost",
+    DRAGON: "dragon",
+}
