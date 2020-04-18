103b5a5286a2486e22b282c9e31c7298ab4eac60
Vanessa Ciputra <38397520+vee-christella@users.noreply.github.com>
2020-04-18 15:31:31 +1200

    Feature/issue 56 map pokemon types and colors (#70)
    * Put pokemon call into Store, and dynamically present cards in pokedex
    
    * Style text for each Pokemon card
    
    * Clean up redux
    
    * Remove console.log test
    
    * Add types to each pokemon
    
    * Simplified types to only have name rather than other attributes
    
    * Ensure pokemon are only loaded once
    
    * Added Label component to each pokemon card
    
    * add type enums and start color
    
    * Labels now use constants and are mapped
    
    * Map pokemon types to card colors
    
    * ensure navigating back to pokedex renders pokemon
    
    * change comment about reducer

--- pokematic-frontend/src/constants.js
+++ pokematic-frontend/src/constants.js
@@ -9,5 +9,29 @@
 
 export const STATUSLIST = ["TODO", "In Progress", "In Review", "Done"];
 
+export const GETGOALS = "";
+export const GETTASKS = "";
+
+export const POKEMONTYPES = {
+    FIRE: "fire",
+    WATER: "water",
+    GRASS: "grass",
+    POISON: "poison",
+    FLYING: "flying",
+    BUG: "bug",
+    NORMAL: "normal",
+    ELECTRIC: "electric",
+    GROUND: "ground",
+    FAIRY: "fairy",
+    FIGHTING: "fighting",
+    PSYCHIC: "psychic",
+    ROCK: "rock",
+    STEEL: "steel",
+    ICE: "ice",
+    GHOST: "ghost",
+    DRAGON: "dragon",
+}
+
 // API CALLS 
 export const HOST = "https://pokematicapi.azurewebsites.net/api/";
+
--- pokematic-frontend/src/pages/pokedex-components/PokedexItem.js
+++ pokematic-frontend/src/pages/pokedex-components/PokedexItem.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import './PokedexItem.css'
+import Label from '../board-components/Label';
+import PokemonMappingUtil from './PokemonMappingUtil';
 
-const PokedexItem = props => {
+class PokedexItem extends React.Component{
+
+  render() {
+
+    const firstType = this.props.pokemonType[this.props.pokemonNumber] && this.props.pokemonType[this.props.pokemonNumber][1][1];
+    const secondType = this.props.pokemonType[this.props.pokemonNumber] && this.props.pokemonType[this.props.pokemonNumber][1][0];
   return (
     <div className="PokedexItem">
       <div className="pokeball-background"></div>
       <div className="background-enabler">
         <div className="pokemon-number">
-            #{props.pokemonNumber}
-          </div>
-          <div className="pokemon-name">
-            {props.pokemonName}
-          </div>
-          <div className="pokemon-image-wrapper">
-            <img className="pokemon-image" src={props.pokemonImage} alt=""></img>
-          </div>
-          <div className="pokemon-type">
-          {/* TODO: change these into separate labels rather than text */}
-          {props.pokemonType[props.pokemonNumber] && props.pokemonType[props.pokemonNumber][1][0]}
-          {props.pokemonType[props.pokemonNumber] && props.pokemonType[props.pokemonNumber][1][1]}
-          </div>
+          #{this.props.pokemonNumber + 1}
         </div>
+        <div className="pokemon-name">
+          {this.props.pokemonName}
+        </div>
+        <div className="pokemon-image-wrapper">
+          <img className="pokemon-image" src={this.props.pokemonImage} alt=""></img>
+        </div>
+        <div className="pokemon-type">
+          {/* TODO: change these into separate labels rather than text */}
+          {firstType ?
+            <Label labelText={firstType} color={PokemonMappingUtil.mapTypeToLabelColors(firstType)}/>
+            : null}
+          <Label labelText={secondType} color={PokemonMappingUtil.mapTypeToLabelColors(secondType)}/>
+        </div>
+      </div>
     </div>
   );
+}
 }
 
 export default PokedexItem;
