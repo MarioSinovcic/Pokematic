3953cf7d0c5ba870aace0d831ba342baea365e1a
Sean Spires <34573028+SeanSpires@users.noreply.github.com>
2020-04-18 15:26:40 +1200

    updated put endpoint so taskname can be properly passed into method

--- pokematic-backend/Controllers/TeamController.cs
+++ pokematic-backend/Controllers/TeamController.cs
@@ -143,7 +143,7 @@ namespace pokematic_backend.Controllers
             return NotFound(serviceMessage);
         }
 
-        [HttpPut("updateTask/{teamName}/{goalName}/{taskName}")]
+        [HttpPut("updateTask/{teamName}/{goalName}/{taskToUpdateName}")]
         public ActionResult UpdateTask(string teamName, string goalName, string taskToUpdateName, Task updatedTask)
         {
             var serviceMessage = _teamService.UpdateTask(teamName, goalName, taskToUpdateName, updatedTask);
