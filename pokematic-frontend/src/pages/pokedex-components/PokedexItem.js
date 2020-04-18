964d86a5f986f9245977c29641882d4ac135031e
Vanessa Ciputra <vee.christella@gmail.com>
2020-04-18 19:47:26 +1200

    Check types for names rather than index

--- pokematic-frontend/src/pages/pokedex-components/PokedexItem.js
+++ pokematic-frontend/src/pages/pokedex-components/PokedexItem.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import './PokedexItem.css'
 
-const PokedexItem = props => {
-  return (
-    <div className="PokedexItem">
-      <div className="pokeball-background"></div>
-      <div className="background-enabler">
-        <div className="pokemon-number">
-            #{props.pokemonNumber}
+class PokedexItem extends React.Component {
+
+  getPokemonTypes(pokemonTypesList) {
+    const typesArray = pokemonTypesList.find(pokemon => 
+      this.props.pokemonName === pokemon[0]
+    )
+
+    if (typesArray) {
+      return typesArray[1];
+    } 
+
+  }
+
+  render() {
+
+    return (
+      <div className="PokedexItem">
+        <div className="pokeball-background"></div>
+        <div className="background-enabler">
+          <div className="pokemon-number">
+            #{this.props.pokemonNumber}
           </div>
           <div className="pokemon-name">
-            {props.pokemonName}
+            {this.props.pokemonName}
           </div>
           <div className="pokemon-image-wrapper">
-            <img className="pokemon-image" src={props.pokemonImage} alt=""></img>
+            <img className="pokemon-image" src={this.props.pokemonImage} alt=""></img>
           </div>
           <div className="pokemon-type">
-          {/* TODO: change these into separate labels rather than text */}
-          {props.pokemonType[props.pokemonNumber] && props.pokemonType[props.pokemonNumber][1][0]}
-          {props.pokemonType[props.pokemonNumber] && props.pokemonType[props.pokemonNumber][1][1]}
+          {/* Change these to labels and pass the text in as props */}
+            {this.getPokemonTypes(this.props.pokemonType) && this.getPokemonTypes(this.props.pokemonType)[1]}
+            {this.getPokemonTypes(this.props.pokemonType) && this.getPokemonTypes(this.props.pokemonType)[0]}
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default PokedexItem;
