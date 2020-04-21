2a0224cc2741c418090a79d74af47e547d198ef5
Vanessa Ciputra <38397520+vee-christella@users.noreply.github.com>
2020-04-21 13:08:38 +1200

    Feature/issue 57 filter pokedex by team collections (#78)
    * color mappings now work
    
    * refactor so each type logic goes into PokedexList
    
    * Refactor and implemented way to change pokemon collection
    
    * store collection in redux so navigating won't have to reload
    
    * Added switch! Now toggles pokemon and collections
    
    * Retain state (collection or all pokemon) for pokedex
    
    * formatting
    
    * remove bg
    
    * move functions to apihandler
    
    * remove console.log

--- pokematic-frontend/src/pages/pokedex-components/PokedexList.js
+++ pokematic-frontend/src/pages/pokedex-components/PokedexList.js
@@ -2,73 +2,9 @@ import React from 'react';
 import PokedexItem from './PokedexItem'
 import './PokedexList.css'
 import { connect } from 'react-redux';
-import { addPokemonNames, addPokemonTypes, togglePokemonLoad, addPokemonData } from '../../actions/actions'
 import PokedexMappingUtil from './PokemonMappingUtil';
 
 class PokedexList extends React.Component {
-
-  state = {
-    pokemon: this.props.pokemonData || [],
-  }
-
-  componentWillMount() {
-    if (!this.props.isLoaded) {
-      this.fetchPokemonData();
-      this.props.togglePokemonHasLoaded();
-    }
-  }
-
-
-  async fetchPokemonData() {
-
-    // Get a list of pokemon Names and their URLs
-    await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
-      .then(response => response.json())
-      .then(data => {
-        this.props.addPokemon(data.results);
-      })
-
-    await this.props.pokemonMap.map((pokemon, i) => {
-      return (
-        this.fetchPokemonTypes(pokemon.url)
-      )
-    })
-
-    // Add data to Redux Store
-    this.props.addPokemonData(this.populatePokemon());
-
-
-    this.setState({
-      pokemon: this.populatePokemon()
-    })
-  }
-
-  async fetchPokemonTypes(pokemonURL) {
-    await fetch(pokemonURL)
-      .then(response => response.json())
-      .then(data => {
-        const name = data.name;
-        const types = [data.types[0].type.name, (data.types[1] && data.types[1].type.name)];
-        this.props.addPokemonTypes(name, types);
-      })
-  }
-
-  populatePokemon() {
-
-    var populatedPokemon = []
-
-    this.props.pokemonMap.map((pokemonData, i) => {
-      return (
-        populatedPokemon.push(
-          [i,
-            pokemonData && pokemonData.name,
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (i + 1) + ".png",
-          ])
-      );
-    })
-    return populatedPokemon;
-  }
-  
 
   getPokemonTypes(pokemonTypesList, pokemonName) {
     const typesArray = pokemonTypesList.find(pokemon =>
@@ -80,21 +16,17 @@ class PokedexList extends React.Component {
       return typesArray[1];
     }
   }
-
-
-
+  
   render() {
     return (
       <div className="PokedexList">
         <div className="grid-container">
-          {this.state.pokemon.map((pokemonData, i) => {
+          {this.props.pokemonCollection.map((pokemonData, i) => {
 
             const pokemonName = pokemonData[1];
             const pokemonTypeList = this.getPokemonTypes(this.props.pokemonTypes, pokemonName);
             
             return (
-
-
               <div key={i} className="grid-item" style={{
                 backgroundColor: PokedexMappingUtil.mapTypeToBackgroundColors(
                   pokemonTypeList ?
@@ -123,28 +55,8 @@ class PokedexList extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    pokemonMap: state.pokemonInfo,
     pokemonTypes: state.pokemonTypes,
-    pokemonData: state.pokemonData,
-    isLoaded: state.isLoaded,
   };
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    togglePokemonHasLoaded: () => {
-      dispatch(togglePokemonLoad())
-    },
-    addPokemonData: (pokemon) => {
-      dispatch(addPokemonData(pokemon))
-    },
-    addPokemon: (pokemon) => {
-      dispatch(addPokemonNames(pokemon))
-    },
-    addPokemonTypes: (pokemonName, pokemonType) => {
-      dispatch(addPokemonTypes(pokemonName, pokemonType))
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PokedexList);
+export default connect(mapStateToProps)(PokedexList);
