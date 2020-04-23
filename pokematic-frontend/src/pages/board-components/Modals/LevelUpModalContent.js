b5aa5f1a5e9f15a1d13cf90734eb75d4970cb2f0
Vanessa Ciputra <vee.christella@gmail.com>
2020-04-23 15:54:35 +1200

    Created modal for level up

---
+++ pokematic-frontend/src/pages/board-components/Modals/LevelUpModalContent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./TaskModalContent.css"
+import "./LevelUpModalContent.css"
+
+function LevelUpModalContent (props) {
+
+    return (
+        <div className="modal-content">
+            <div className="title">
+            Level Up!
+            </div>
+        </div>
+    );
+}
+export default LevelUpModalContent;
