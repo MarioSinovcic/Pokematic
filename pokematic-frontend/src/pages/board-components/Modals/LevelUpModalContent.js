5d4b57024f57e4a2f28c69608aab6546c59e0b62
Vanessa Ciputra <vee.christella@gmail.com>
2020-04-24 19:43:59 +1200

    Styled static reward popup

--- pokematic-frontend/src/pages/board-components/Modals/LevelUpModalContent.js
+++ pokematic-frontend/src/pages/board-components/Modals/LevelUpModalContent.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import "./TaskModalContent.css"
 import "./LevelUpModalContent.css"
+import { Button } from '@material-ui/core';
+import { useHistory} from "react-router-dom";
 
-function LevelUpModalContent (props) {
 
-    return (
-        <div className="modal-content">
-        <div className="blocks">
-            <div className="new-level">LV. 21</div>
-            <div className="title">
-            Level Up!
+class LevelUpModalContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        
+        this.state = {
+            showReward: false,
+            rewardName: "",
+            newLevel: 21,
+        }
+    }
+
+    switchToReward() {
+        this.setState({
+            showReward: true,
+            // Temporary static values
+            rewardName: "Squirtle",
+        })
+
+    }
+
+    toPokedex() {
+        const history = useHistory();
+        history.push("/pokedex");
+    }
+
+
+    render() {
+        return (
+            <div className="modal-content">
+                <div className="blocks">
+                    <div className="new-level">LV. {this.state.newLevel}</div>
+                    <div className="title">
+                        {!this.state.showReward ? "Level Up!" : this.state.rewardName }
             </div>
-            <div className="pokeball"><img src="/images/pokeballPrize.png" alt="prize" className="pokeball" onClick={props.handleClose}/></div>
-        </div>
-        </div>
-    );
+                    {!this.state.showReward ?
+                        <div className="pokeball"><img src="/images/pokeballPrize.png" alt="prize" className="pokeball hidden" onClick={this.switchToReward.bind(this)} /></div>
+                        : <div className="pokeball">
+                            <div className="reward-circle pokeball">
+                                {/* //TODO: Change this image to dynamically change depending on the pokemon received */}
+                                    <img className="pokemon-image pokemon-reward" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt=""></img>
+                                
+                            </div>
+                            <div className="action-button-container">
+                                <Button className="reward-button" onClick={() => this.toPokedex()} >View Collection</Button>
+                                <Button className="reward-button" onClick={this.props.handleClose}>Done</Button>
+                            </div>
+
+                        </div>}
+                </div>
+            </div>
+        );
+    }
 }
 export default LevelUpModalContent;
