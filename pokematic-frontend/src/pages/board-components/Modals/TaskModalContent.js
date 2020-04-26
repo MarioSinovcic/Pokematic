f8c316327dcaa8bac02010550e009cc91870fe5b
Mario Sinovcic <38930019+MarioSinovcic@users.noreply.github.com>
2020-04-26 15:29:44 +1200

    enhancement/issue-95-Add-Approval-Functionality  (#97)

--- pokematic-frontend/src/pages/board-components/Modals/TaskModalContent.js
+++ pokematic-frontend/src/pages/board-components/Modals/TaskModalContent.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import {deleteTask} from '../../../apiHandler';
+import React, { useState } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Typography } from '@material-ui/core';
+import {deleteTask, updateTask} from '../../../apiHandler';
 import "./TaskModalContent.css"
 
 function TaskModalContent (props) {
+    var disableCheckBox = false;
+    var defaultStatus = false;
+    const [showBinButton, setShowBinButton] = useState(true);
+    const [selectedApproved, setSelectedApproved] = useState(false);
 
-    async function handleApprove() {
-        //TODO: approveTask();
-    };
+    if(props.status !== "Done"){
+        disableCheckBox = true;
+    }
+
+    if(props.approved){
+        disableCheckBox = true;
+        defaultStatus= true; 
+    }
+
+    function handleCheckBox(){
+        setSelectedApproved(!selectedApproved);
+        setShowBinButton(!showBinButton);
+    }
 
     async function handleDelete() {
         await deleteTask(props.goalName, props.name);
@@ -14,11 +31,28 @@ function TaskModalContent (props) {
         props.handleClose();
     };
 
+    async function handleSave() {
+        const updatedTask = {
+            name: props.name,
+            taskNumber: props.taskNumber, 
+            description: props.description,
+            experiencePoints: parseInt(props.storyPoints),
+            status: props.status,
+            storyPoints: parseInt(props.storyPoints),
+            assignees: props.assignees, 
+            approved: selectedApproved,
+        };
+        await updateTask(updatedTask, props.goalName, props.name);
+        await props.populatePage();
+
+        props.handleClose();
+    };
+
     return (
         <div className="modal-content">
             <div className="grouping">
                 <p className="task-number">#{props.taskNumber}</p>
-                <p className="task-title">{props.name}</p>
+                <p className="modal-task-title">{props.name}</p>
             </div>    
             <div className="grouping padding-bottom-p">
                 <p className="goal-name">{props.goalName}</p>
@@ -32,9 +66,20 @@ function TaskModalContent (props) {
                 <p className="description">{props.description}</p>
             </div>  
             <div className="grouping">
-                <button className="approve-button" onClick={handleApprove}>APPROVE</button>
+                <div className="approved-section">
+                    <FormControlLabel
+                        disabled={disableCheckBox}
+                        value="start"
+                        control={<Checkbox color={"default"} onChange={handleCheckBox} defaultChecked={defaultStatus}/>}
+                        label={
+                            <Typography className="approved-label"> APPROVED? </Typography>
+                        }
+                        labelPlacement="start"
+                    />
+                </div>
                 <div className="right-align">
-                    <button className="bin-button" onClick={handleDelete}/>
+                    {showBinButton && <button className="bin-button" onClick={handleDelete}/>}
+                    {!showBinButton && <button className="save-button" onClick={handleSave}>SAVE</button>}
                 </div>
             </div> 
         </div>
