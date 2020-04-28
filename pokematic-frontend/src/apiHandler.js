9b0d17dbefcfe26b30686e10e1b1d00fd7423095
Vanessa Ciputra <vee.christella@gmail.com>
2020-04-28 12:41:04 +1200

    Fix modal sizing bug
    This was caused by sharing the same css name with the level up modals

--- pokematic-frontend/src/apiHandler.js
+++ pokematic-frontend/src/apiHandler.js
@@ -12,7 +12,7 @@
 import {STATUSLIST} from './constants';
 import {HOST} from './constants';
 
-var teamName = "Dummy Team2"; //temporary
+var teamName = "BE Team"; //temporary
 
 export async function createTask(newTask, goalName){
     var APIcall = HOST + "team/createTask/" + teamName + "/" + goalName;
