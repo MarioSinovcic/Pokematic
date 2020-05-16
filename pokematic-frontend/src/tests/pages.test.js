8e417e5b5ac3878b688bcbed9ab77ea60b5e5d73
Vanessa Ciputra <vee.christella@gmail.com>
2020-05-16 12:46:37 +1200

    Create snapshot tests for board components

--- pokematic-frontend/src/tests/pages.test.js
+++ pokematic-frontend/src/tests/pages.test.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Board from '../pages/Board';
-import Login from '../pages/Login';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { createStore } from 'redux';
 import rootReducers from '../reducers';
-import TeamCard from '../shared-components/TeamCard';
 
 //The following code includes examples for snapshot testing components.
 //Please try following the naming convetions shown in these examples
 
 //Docs : https://enzymejs.github.io/enzyme/
 
+xdescribe('Board', () => {
 
+    
+
+    //this tests if the component renders without errors
+    it('should render without throwing an error', () => {
+        jest.mock('react-router-dom', () => ({
+            useHistory: () => ({
+              push: jest.fn(),
+            }),
+          }));
+          
+        const store = createStore(
+            rootReducers,
+        );
+        const result = renderer.create(
+            <Provider store={store}>
+        
+            <Board />
+            </Provider>
+          ).toJSON();
+          expect(result).toMatchSnapshot();
+    })
+
+    // //this tests if the Board component renders only one div with the classname Board (hence div.Board)
+    // it('should render an encapsilating div', () => {
+    //     expect(shallow(<Board />).find('div.Board').length).toEqual(1)
+    // });
+
+    // //this tests if the component renders correctly without props
+    // it('should render correctly in with no props', () => {
+    // //   const component = shallow(<Board/>);
+    
+    // //   expect(component).toMatchSnapshot();
+    //     expect(this)
+
+    // });
+
+    // //this tests if the component renders correctly with props
+    // it('should render correctly in with props', () => {
+    //     const inputProps = ['one', 'two'];
+
+    //     const component = shallow(<Board list={inputProps}/>);
+          
+    //     expect(component).toMatchSnapshot();
+    // });
+});
