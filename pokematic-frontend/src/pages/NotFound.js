085981ce263ed38bcad5382239c387b2a35bc706
Vanessa Ciputra <38397520+vee-christella@users.noreply.github.com>
2020-05-24 17:10:01 +1200

    Fix Tests (#171)
    * Fix page tests
    
    * Refactor unused files and update snapshots
    
    * Remove unused README
    
    * Remove accidental commit of workspace.xml and .idea folder

--- pokematic-frontend/src/pages/NotFound.js
+++ pokematic-frontend/src/pages/NotFound.js
@@ -5,7 +5,7 @@ function NotFound() {
     <div className="App">
       <header className="App-header">
         <p>
-          404 Cheif
+          404: You've navigated to a non-existing page of Pokematic!
         </p>
       </header>
     </div>
